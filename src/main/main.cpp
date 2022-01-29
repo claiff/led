0c66cae87bf155566d1adbe9bcb6922bba815081
eo.portnov <eo.portnov@tensor.ru>
2022-01-29 23:16:00 +0300

    Пока просто в main собираю led_matrix

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,14 +1,20 @@
 #include "periphery/rcc_helper.hpp"
 #include "periphery/pwm.hpp"
 #include "periphery/timer_irq.hpp"
+#include "device/led_matrix.hpp"
+
+static constexpr uint8_t WIDTH = 16;
+static constexpr uint8_t HEIGHT = 16;
 
 int main( )
 {
 	periphery::RccHelper rcc;
 	rcc.SetMaxRcc();
 
-	periphery::PWM pwm{rcc, 1};
-	periphery::TimerIRQ timer_irq{pwm};
+	//FIXME Как то объединить
+	periphery::PWM pwm{rcc, WIDTH * HEIGHT};
+	periphery::TimerIRQ irq{pwm};
+	device::LedMatrix led{HEIGHT,WIDTH, pwm};
 
 	pwm.StartPWM();
 	while(1)
